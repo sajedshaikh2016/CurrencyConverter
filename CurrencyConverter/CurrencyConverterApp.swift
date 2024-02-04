460e2c85cc271933c99e97a5aca8e7035b5ef039
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-02-04 17:52:20 +0530

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Sajed Shaikh on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
